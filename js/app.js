82f622acbfe3984b74e5e31a423583f859487584
Adriaan van der Ploeg <adriaanvanderploeg@gmail.com>
2019-07-15 13:58:34 +0200

    added eventlisterner to overlay changed cursor on hover

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 const qwerty= document.querySelector('#qwerty');
 const phrase= document.querySelector('#phrase');
 let missed= 0;
+
+const startGame= document.querySelector('.btn__reset');
+const overlay= document.querySelector('#overlay');
+
+startGame.style.cursor ='pointer';
+
+startGame.addEventListener('click', ()=>{
+  overlay.style.display= 'none';
+});
