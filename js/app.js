3a65dbcd05d07d42b9fddcd69fab613baf88130c
Adriaan van der Ploeg <adriaanvanderploeg@gmail.com>
2019-07-15 14:02:17 +0200

    added array

--- js/app.js
+++ js/app.js
@@ -5,6 +5,15 @@ let missed= 0;
 const startGame= document.querySelector('.btn__reset');
 const overlay= document.querySelector('#overlay');
 
+const phrases=[
+'you will never guess the right answer',
+'you will fail this game',
+'i will win everytime',
+'i will win every game we will ever play',
+'you will  lose every game we will ever play'
+];
+
+
 startGame.style.cursor ='pointer';
 
 startGame.addEventListener('click', ()=>{
