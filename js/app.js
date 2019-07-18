1577d7ac79efce9e741cebec858ad3bc2df3f519
Adriaan van der Ploeg <adriaanvanderploeg@gmail.com>
2019-07-18 09:32:41 +0200

    added checkletter/qwerty.eventlistener

--- js/app.js
+++ js/app.js
@@ -1,19 +1,17 @@
 const qwerty= document.querySelector('#qwerty');
 const phrase= document.querySelector('#phrase');
+
 let missed= 0;
+let hearts= document.querySelectorAll('img');
 
 let char= [];
 
 const startGame= document.querySelector('.btn__reset');
 const overlay= document.querySelector('#overlay');
 
-const phrases=[
-'you will never guess the right answer',
-'you will fail this game',
-'i will win everytime',
-'i will win every game we will ever play',
-'you will  lose every game we will ever play'
-];
+
+
+
 
 
 function getRandomPhraseAsArray(arr){
@@ -24,26 +22,72 @@ function getRandomPhraseAsArray(arr){
 }
 
 
+
+
+
+
 function addPhraseToDisplay(arr){
   for(let i=0;i<char.length;i++){
     const li= document.createElement('li');
     const attach= document.querySelector('#phrase > ul');
+
+      if(char[i]===" "){
+        li.style.display= 'none';//should add whitespace
+      }else{
+        li.className='letter';
+      }
+
     li.textContent=char[i];
+    attach.appendChild(li);
+  }
+  console.log(arr)
+}
 
-    if(li===''){
-      li.style.display= 'none';
-    }else{
-      li.className='letter';
-      attach.appendChild(li);
+
+
+function checkLetter(btn){
+
+
+let inputLetter= document.querySelectorAll('.letter');
+let match= null;
+
+  for(let i=0;i<inputLetter.length;i++){
+    if(inputLetter[i].textContent===btn.textContent){
+      inputLetter[i].className+=' show';
+     match= true;
     }
+  }
+  return match;
+}
 
 
 
+
+
+qwerty.addEventListener('click',(e)=>{
+  if(e.target && e.target.nodeName=='BUTTON'){
+    e.target.className='chosen';
+    e.target.setAttribute('disabled', '');
+    let letterFound=checkLetter(e.target);
+
+      if(letterFound===null){
+
+        for(let i=0; i<hearts.length;i++){
+
+          this.hearts[i].src='images/lostHeart.png';
+        }
+
+
+        missed--;
+        }else{
+          missed++;
+    }
   }
 
 
-  console.log(arr)
-}
+
+})
+
 
 
 
@@ -54,7 +98,6 @@ startGame.style.cursor ='pointer';
 startGame.addEventListener('click', ()=>{
   overlay.style.display= 'none';
 });
-
 
 
 
---
+++ js/phrases.js
@@ -0,0 +1,7 @@
+const phrases=[
+'you will never guess the right answer',
+'you will fail this game',
+'i will win everytime',
+'i will win every game we will ever play',
+'you will  lose every game we will ever play'
+];
